008d562d63d7e42f4a392dfd67a0949bc069fc45
Dogodogo11 <shota.dogo0084@gmail.com>
2024-10-29 14:00:50 +0900

    問題画面の作り直し

--- Question.xaml.cs
+++ Question.xaml.cs
@@ -32,7 +32,7 @@ namespace HitApp
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-           
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -46,3 +46,4 @@ namespace HitApp
         }
     }
 }
+
