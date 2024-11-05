2d23d2668929677e81efd091523dbfc8036e77e1
“214035” <214035@ocsjoho.onmicosoft.com>
2024-11-05 10:01:18 +0900

    問題分の表示

--- Question.xaml.cs
+++ Question.xaml.cs
@@ -16,26 +16,51 @@ using System.Windows.Shapes;
 
 namespace HitApp
 {
+
     /// <summary>
     /// Medical_Q.xaml の相互作用ロジック
     /// </summary>
     public partial class Question : Page
     {
+        // 問題分、選択肢を格納するリスト
+        List<string> QList = new List<string>();
+        // 解いている問題が何問目か
+        int QCount = 0;
+        // 正解数
+        int rightCount = 0;
+
         public Question(string year, string bunnya)
         {
             InitializeComponent();
             title.Text = year + "年度・" + bunnya;
 
             getText();
+            display();
         }
 
         private void getText ()
         {
             StreamReader csv = new StreamReader(@"../../csv/2019JS.csv");
-            string line = csv.ReadLine();
-            string[] values = line.Split(',');
 
-            monndai.Text = values[0];
+            csv.ReadLine();// 一行とばす
+            while (!csv.EndOfStream)
+            {
+                string line = csv.ReadLine();
+                string[] values = line.Split(',');
+
+                QList.AddRange(values);
+            }
+        }
+
+        private void display()
+        {
+            Qnum.Text = " 問" + QList[0];
+            monndai.Text = QList[1];
+            selection1.Text = QList[2];
+            selection2.Text = QList[3];
+            selection3.Text = QList[4];
+            selection4.Text = QList[5];
+            selection5.Text = QList[6];
         }
     }
 }
